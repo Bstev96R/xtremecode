7d093c6ea3977131a86fd94e588024b732c90a38
Bstev96R <bstev96r@gmail.com>
2019-11-27 11:16:53 -0600

    nuevo projecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { ImageComponent } from './images/image/image.component';
+import { ImageListComponent } from './images/image-list/image-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import {environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {ReactiveFormsModule} from '@angular/forms';
+  import { from } from 'rxjs';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent,
+    ImageComponent,
+    ImageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBBjpoJaMAIEV9ff8flJqgR4hLSm5-2-So",
+    authDomain: "suganic-2cf32.firebaseapp.com",
+    databaseURL: "https://suganic-2cf32.firebaseio.com",
+    projectId: "suganic-2cf32",
+    storageBucket: "suganic-2cf32.appspot.com",
+    messagingSenderId: "791719846043",
+    appId: "1:791719846043:web:629458b111414ac579e63e",
+    measurementId: "G-2Y7D6MT8XK"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
