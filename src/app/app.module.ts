6da38aaf825e9d8cc4f46096d869315482053246
Berflo26 <b.valleflores@gmail.com>
2019-12-01 22:24:36 -0600

    más errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ImagesComponent } from './images/images.component';
-import { ImageComponent } from './images/image/image.component';
-import { ImageListComponent } from './images/image-list/image-list.component';
-import { AppRoutingModule } from './app-routing.module';
-import {environment} from '../environments/environment';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import {ReactiveFormsModule} from '@angular/forms';
-import { from } from 'rxjs';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './Login/login.component';
-import { RegistroComponent } from './Registro/registro.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AppComponent } from "./app.component";
+import { ImagesComponent } from "./images/images.component";
+import { ImageComponent } from "./images/image/image.component";
+import { ImageListComponent } from "./images/image-list/image-list.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { ReactiveFormsModule } from "@angular/forms";
+import { from } from "rxjs";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { LoginComponent } from "./Login/login.component";
+import { RegistroComponent } from "./Registro/registro.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -36,9 +35,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     BrowserAnimationsModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
