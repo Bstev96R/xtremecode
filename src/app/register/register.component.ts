621f25ea93724f3ca939d90e843bf65dd8131c44
Berflo26 <b.valleflores@gmail.com>
2019-12-02 04:43:00 -0600

    prueba

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,8 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
   }
-
 }
+
+
