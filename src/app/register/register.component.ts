16649fc5becc8f76e7dab0ef1446f9cfd6b0b04a
Berflo26 <b.valleflores@gmail.com>
2019-12-02 05:33:57 -0600

    pruebas

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   constructor() { }
   ngOnInit() {
   }
