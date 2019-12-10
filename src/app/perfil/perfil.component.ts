f062c4e9b046beeb7bb9a38aada46cdbc5f78eb1
Berflo26 <b.valleflores@gmail.com>
2019-12-10 11:12:07 -0600

    profile user conflict

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,7 @@
+import { UserInterface } from './../models/user';
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  user: UserInterface = {
+    name:'',
+    email: '',
+    photoUrl:''
+  };
+
+  public providerId: string = 'null';
 
   ngOnInit() {
+    this.authService.isAuth().subscribe(user => {
+      if (user) {
+        this.user.name = user.displayName;
+        this.user.email = user.email;
+        this.user.photoUrl = user.photoURL;
+        this.providerId = user.providerData[0].providerId;
+      }
+    })
   }
 
 }
