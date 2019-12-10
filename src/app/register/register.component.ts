4b03742e8d4e8f663bd2dc5fdf76ecf7dd289eaf
Berflo26 <b.valleflores@gmail.com>
2019-12-10 01:58:41 -0600

    finalizado2

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 
 
@@ -8,8 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor() { }
+
+  constructor(private router: Router, private authService: AuthService) { }
   ngOnInit() {
+  }
+  displayForm: boolean = false;
+  public email: string= '';
+  public password: string='';
+  onAddUser(){
+    this.authService.RegisterUser(this.email, this.password)
+    .then((res)=>{
+      this.router.navigate(['perfil']);
+    }).catch(err => console.log('err', err.message));
+  }
+
+  onLoginGoogle(): void {
+    //this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.authService
+      .LoginGoogleUser()
+      .then(res => {
+        console.log("resUser", res);
+        this.onLoginRedirect();
+      })
+      .catch(err => console.log("err", err.message));
+  }
+
+
+  onLoginRedirect(): void {
+    this.router.navigate(["perfil"]);
   }
 }
 
