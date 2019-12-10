a6bfd96303e198cd7c2e308010af8900d438a6ca
Berflo26 <b.valleflores@gmail.com>
2019-12-09 22:10:40 -0600

    mejoras

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,48 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import { Router } from '@angular/router';
-import {AuthService} from '../services/auth.service';
-import { NgForm } from '@angular/forms/src/directives/ng_form';
-
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { NgForm } from "@angular/forms/src/directives/ng_form";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth, private route: Router, private authService: AuthService) {
 
-   }
+  constructor(
+    public afAuth: AngularFireAuth,
+    private route: Router,
+    private authService: AuthService
+  ) {}
 
-   public email : "";
-   public password : "";
-  ngOnInit() {
-  }
+  public email: string = "";
+  public password: string = "";
+  ngOnInit() {}
 
   onLogin(): void {
-    console.log('email', this.email);
-    console.log('password', this.password);
-
-    /*this.authService.RegisterUser(this.email, this.password)
-    .then((res)=> {
-      this.route.navigate(['perfil']);
-    }).catch(err => console.log('err', err.message));*/
+    console.log("email", this.email);
+    console.log("password", this.password);
+    this.authService
+      .LoginEmailPassUser(this.email, this.password)
+      .then(res => {
+        this.route.navigate(["perfil"]);
+      })
+      .catch(err => console.log("err", err.message));
   }
 
   onLoginGoogle(): void {
     //this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
-    this.authService.LoginGoogleUser()
-    .then((res) =>{
-    this.route.navigate(['perfil']);
-    }).catch(err => console.log('err', err.message));
+    this.authService
+      .LoginGoogleUser()
+      .then(res => {
+        console.log("resUser", res);
+        this.route.navigate(["perfil"]);
+      })
+      .catch(err => console.log("err", err.message));
   }
 
-  onLogoutGoogle(){
+  onLogoutGoogle() {
     // this.afAuth.auth.signOut();
     this.authService.LogoutUser();
   }
 }
-
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,46 +1,44 @@
-import { Injectable } from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {map} from 'rxjs/operators';
-import {auth} from 'firebase/app';
-import { promise } from 'protractor';
-import { resolve } from 'url';
-import { reject } from 'q';
-import { error } from 'util';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { map } from "rxjs/operators";
+import { auth } from "firebase/app";
+import { promise } from "protractor";
+import { resolve } from "url";
+import { reject } from "q";
+import { error } from "util";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  constructor(private afsAuth: AngularFireAuth) {}
 
-  constructor(private afsAuth: AngularFireAuth ) { }
-
-  RegisterUser(email:string, pass: string){
+  /*RegisterUser(email:string, pass: string){
     return new Promise((resolve, reject)=>{
       this.afsAuth.auth.signInWithEmailAndPassword(email, pass)
       .then(userData => resolve(userData),
       err => reject(err));
     }) 
-  }
-
-  LogoutUser(){
-    return this.afsAuth.auth.signOut();
-
-  }
-
-  LoginGoogleUser(){
+  }*/
+  LoginGoogleUser() {
     return this.afsAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
   }
- 
-  LoginFacebookUser(){
 
+  LoginFacebookUser() {}
+
+  LoginEmailPassUser(email: string, pass: string) {
+    return new Promise((resolve, reject) => {
+      this.afsAuth.auth.signInWithEmailAndPassword(email, pass).then(
+        userData => resolve(userData),
+        err => reject(err)
+      );
+    });
   }
 
-  LoginEmailPassUser(email: string, pass: string){
-
+  LogoutUser() {
+    return this.afsAuth.auth.signOut();
   }
-
-  isAuth(){
-    return this.afsAuth.authState.pipe(map (auth => auth));
-
+  isAuth() {
+    return this.afsAuth.authState.pipe(map(auth => auth));
   }
 }
